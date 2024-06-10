c3183b36e2db17986f4c7117b99c2d185ce9d106
ADMIN <ADMIN@LAB1-MAY22>
2024-06-10 10:36:29 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace c_hinhtron1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btndientich = new System.Windows.Forms.Button();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtkq = new System.Windows.Forms.TextBox();
+            this.btnchuvi = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rdbdt1 = new System.Windows.Forms.RadioButton();
+            this.rdbcv1 = new System.Windows.Forms.RadioButton();
+            this.chkdt2 = new System.Windows.Forms.CheckBox();
+            this.chkcv2 = new System.Windows.Forms.CheckBox();
+            this.btntinh1 = new System.Windows.Forms.Button();
+            this.btntinh2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btndientich
+            // 
+            this.btndientich.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndientich.Location = new System.Drawing.Point(42, 240);
+            this.btndientich.Name = "btndientich";
+            this.btndientich.Size = new System.Drawing.Size(75, 23);
+            this.btndientich.TabIndex = 0;
+            this.btndientich.Text = "DIỆN TÍCH ";
+            this.btndientich.UseVisualStyleBackColor = true;
+            this.btndientich.Click += new System.EventHandler(this.btndientich_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(163, 89);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 20);
+            this.txt1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(89, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(254, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "DIỆN TÍCH CHU VI HÌNH TRÒN ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "NHẬP BÁN KÍNH ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(39, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "KẾT QUẢ ";
+            // 
+            // txtkq
+            // 
+            this.txtkq.Location = new System.Drawing.Point(163, 155);
+            this.txtkq.Name = "txtkq";
+            this.txtkq.Size = new System.Drawing.Size(100, 20);
+            this.txtkq.TabIndex = 5;
+            // 
+            // btnchuvi
+            // 
+            this.btnchuvi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnchuvi.Location = new System.Drawing.Point(163, 240);
+            this.btnchuvi.Name = "btnchuvi";
+            this.btnchuvi.Size = new System.Drawing.Size(75, 23);
+            this.btnchuvi.TabIndex = 6;
+            this.btnchuvi.Text = "CHU VI";
+            this.btnchuvi.UseVisualStyleBackColor = true;
+            this.btnchuvi.Click += new System.EventHandler(this.btnchuvi_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdbcv1);
+            this.groupBox1.Controls.Add(this.rdbdt1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(327, 89);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(197, 127);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "chọn chức năng";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.chkcv2);
+            this.groupBox2.Controls.Add(this.chkdt2);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(553, 94);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(197, 122);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "chọn chức năng";
+            // 
+            // rdbdt1
+            // 
+            this.rdbdt1.AutoSize = true;
+            this.rdbdt1.Location = new System.Drawing.Point(35, 36);
+            this.rdbdt1.Name = "rdbdt1";
+            this.rdbdt1.Size = new System.Drawing.Size(97, 20);
+            this.rdbdt1.TabIndex = 0;
+            this.rdbdt1.TabStop = true;
+            this.rdbdt1.Text = "tính diện tích";
+            this.rdbdt1.UseVisualStyleBackColor = true;
+            // 
+            // rdbcv1
+            // 
+            this.rdbcv1.AutoSize = true;
+            this.rdbcv1.Location = new System.Drawing.Point(35, 79);
+            this.rdbcv1.Name = "rdbcv1";
+            this.rdbcv1.Size = new System.Drawing.Size(100, 20);
+            this.rdbcv1.TabIndex = 1;
+            this.rdbcv1.TabStop = true;
+            this.rdbcv1.Text = "radioButton2";
+            this.rdbcv1.UseVisualStyleBackColor = true;
+            // 
+            // chkdt2
+            // 
+            this.chkdt2.AutoSize = true;
+            this.chkdt2.Location = new System.Drawing.Point(32, 31);
+            this.chkdt2.Name = "chkdt2";
+            this.chkdt2.Size = new System.Drawing.Size(101, 20);
+            this.chkdt2.TabIndex = 0;
+            this.chkdt2.Text = "tính diện tích ";
+            this.chkdt2.UseVisualStyleBackColor = true;
+            // 
+            // chkcv2
+            // 
+            this.chkcv2.AutoSize = true;
+            this.chkcv2.Location = new System.Drawing.Point(32, 75);
+            this.chkcv2.Name = "chkcv2";
+            this.chkcv2.Size = new System.Drawing.Size(83, 20);
+            this.chkcv2.TabIndex = 1;
+            this.chkcv2.Text = "tính chu vi";
+            this.chkcv2.UseVisualStyleBackColor = true;
+            // 
+            // btntinh1
+            // 
+            this.btntinh1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btntinh1.Location = new System.Drawing.Point(387, 257);
+            this.btntinh1.Name = "btntinh1";
+            this.btntinh1.Size = new System.Drawing.Size(75, 23);
+            this.btntinh1.TabIndex = 9;
+            this.btntinh1.Text = "tính 1";
+            this.btntinh1.UseVisualStyleBackColor = true;
+            this.btntinh1.Click += new System.EventHandler(this.btntinh1_Click);
+            // 
+            // btntinh2
+            // 
+            this.btntinh2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btntinh2.Location = new System.Drawing.Point(602, 257);
+            this.btntinh2.Name = "btntinh2";
+            this.btntinh2.Size = new System.Drawing.Size(75, 23);
+            this.btntinh2.TabIndex = 10;
+            this.btntinh2.Text = "tính 2";
+            this.btntinh2.UseVisualStyleBackColor = true;
+            this.btntinh2.Click += new System.EventHandler(this.btntinh2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btntinh2);
+            this.Controls.Add(this.btntinh1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnchuvi);
+            this.Controls.Add(this.txtkq);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.btndientich);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btndientich;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtkq;
+        private System.Windows.Forms.Button btnchuvi;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rdbcv1;
+        private System.Windows.Forms.RadioButton rdbdt1;
+        private System.Windows.Forms.CheckBox chkcv2;
+        private System.Windows.Forms.CheckBox chkdt2;
+        private System.Windows.Forms.Button btntinh1;
+        private System.Windows.Forms.Button btntinh2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace c_hinhtron1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btndientich_Click(object sender, EventArgs e)
+        {
+            Double r, p;
+            r = Convert.ToDouble(txt1.Text);
+
+            p = Math.PI * r * r;
+            txtkq.Text = p.ToString();
+        }
+
+        private void btnchuvi_Click(object sender, EventArgs e)
+        {
+            Double r, p;
+            r = Convert.ToDouble(txt1.Text);
+
+            p = Math.PI * r * 2;
+            txtkq.Text = p.ToString();
+        }
+
+        private void btntinh1_Click(object sender, EventArgs e)
+        {
+            double r=Convert.ToDouble(txt1.Text);
+            
+            if (rdbdt1.Checked)
+            {
+                double dientich = r * r * Math.PI;
+
+                string thongbao = "-diện tích" + Convert.ToString(dientich);
+                MessageBox.Show(thongbao, "thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            if (rdbcv1.Checked)
+            {
+                double chuvi = r * 2 * Math.PI;
+                string thongbao = "-chu vi" + Convert.ToString(chuvi);
+                MessageBox.Show(thongbao, "thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+
+        private void btntinh2_Click(object sender, EventArgs e)
+        {
+            double bankinh=Convert.ToDouble(txt1.Text);
+            string thongbao = "";
+            if (chkdt2.Checked)
+            {
+                double dientich = bankinh * bankinh * Math.PI;
+                thongbao=thongbao+"-dien tich"+Convert.ToString(dientich)+Environment.NewLine;
+            }
+            if (chkcv2.Checked)
+            {
+                double chuvi = bankinh * 2 * Math.PI;
+                thongbao = thongbao + "-chu vi" + Convert.ToString(chuvi) + Environment.NewLine;
+            }
+            if(thongbao.Length > 0)
+            {
+                MessageBox.Show(thongbao,"thông báo",MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("vui lòng chọn tính năng","thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
